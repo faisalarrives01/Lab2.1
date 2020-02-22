8a4b21f13fc3bd97ffbb020a5a3487b523495a32
Ubuntu <n4nisar.ece95@gmail.com>
2020-02-22 13:13:51 +0000

    nisar i

--- hello.java
+++ hello.java
@@ -4,13 +4,18 @@ class HelloJava {
 
 public static void main(String args[]){
 
-
-
+System.out.Println("This is notthe work in the hello.java");
 System.out.Println(“ I am your Java Program. Thank you! ”);
 
 
 
 }
+
+
+
+
+
+
 
 
 
